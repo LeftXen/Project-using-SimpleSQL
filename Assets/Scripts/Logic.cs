3089feafddc81d601576e436908e41166d88d94b
NIKOLAOS SERGIS <78097143+SerNikos@users.noreply.github.com>
2022-11-02 17:14:09 +0200

    first init

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -16,19 +16,19 @@ public class Logic : MonoBehaviour
     {
 
         //Create Table Players
-       /* string sql6;
-        sql6 = "CREATE TABLE \"Players\" " +
-        "(\"PlayerID\" INTEGER PRIMARY KEY NOT NULL, " +
-        "\"PlayerName\" varchar(60), " +
-        "\"Score\" FLOAT";
-        dbManager.Execute(sql6);*/
+        /* string sql6;
+         sql6 = "CREATE TABLE \"Players\" " +
+         "(\"PlayerID\" INTEGER PRIMARY KEY NOT NULL, " +
+         "\"PlayerName\" varchar(60), " +
+         "\"Score\" FLOAT";
+         dbManager.Execute(sql6);*/
 
-       /* string sql1 = "INSERT INTO StarShip " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
-        dbManager.Execute(sql1, 1, "Nikolaos Sergis", 12f);
-        string sql2 = "INSERT INTO StarShip " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
-        dbManager.Execute(sql2, 1, "Lefteris Xenos", 22f);
-        string sql3 = "INSERT INTO StarShip " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
-        dbManager.Execute(sql3, 1, "Konstantinos Doldoukis", 2f);*/
+        /* string sql1 = "INSERT INTO StarShip " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
+         dbManager.Execute(sql1, 1, "Nikolaos Sergis", 12f);
+         string sql2 = "INSERT INTO StarShip " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
+         dbManager.Execute(sql2, 1, "Lefteris Xenos", 22f);
+         string sql3 = "INSERT INTO StarShip " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
+         dbManager.Execute(sql3, 1, "Konstantinos Doldoukis", 2f);*/
         /*
         //print all from StarShip TABLE
         var results = dbManager.Query<SimpleSQL.Demos.Players>("SELECT * FROM StarShip");
@@ -52,102 +52,113 @@ public class Logic : MonoBehaviour
             "\"Firepower\" FLOAT)";
             dbManager.Execute(sql);
         */
-            string sql;
-            sql = "CREATE TABLE \"Players\" " +
-            "(\"PlayerID\" INTEGER PRIMARY KEY NOT NULL, " +
-            "\"PlayerName\" varchar(60), " +
-            "\"Score\" FLOAT)";
+
+
+        /* string sql;
+         sql = "CREATE TABLE \"Players\" " +
+         "(\"PlayerID\" INTEGER PRIMARY KEY NOT NULL, " +
+         "\"PlayerName\" varchar(60), " +
+         "\"Score\" FLOAT)";
+         dbManager.Execute(sql);*/
+
+
+        /* string sql1 = "INSERT INTO Players " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
+         dbManager.Execute(sql1, 1, "Nikolaos Sergis", 12f);
+         string sql2 = "INSERT INTO Players " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
+         dbManager.Execute(sql2, 2, "Lefteris Xenos", 22f);
+         string sql3 = "INSERT INTO Players " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
+         dbManager.Execute(sql3, 3, "Konstantinos Doldoukis", 2f);*/
+
+       /* dbManager.CreateTable<PlayerData>();
+
+        PlayerData playerStats = new PlayerData{ PlayerID = 8, PlayerName = "yolos", Score = 500f};
+        dbManager.Insert(playerStats);
+
+        string sql = "SELECT * FROM Weapon";
+        List<PlayerData> weapons = dbManager.Query<PlayerData>(sql);*/
+
+
+        /*
+                int row = 0;
+                TextOutput.text = ""; 
+                Players dt = dbManager.Query("SELECT " +
+                "W.WeaponID, " +
+                "W.WeaponName, " +
+                "W.Damage, " +
+                "W.Cost, " +
+                "W.Weight, " +
+                "W.WeaponTypeID, " +
+                "T.Description AS
+                WeaponTypeDescription " +
+                "FROM " +
+                "Weapon W " +
+                "JOIN WeaponType T " +
+                "ON W.WeaponTypeID =
+                T.WeaponTypeID " +
+                "ORDER BY " +
+                "W.WeaponID ");
+                        foreach (DataRow dr in dt.Rows)
+                        {
+                            text.text += "Row: " + row.ToString() + " ";
+                            for (int c = 0; c < dt.Columns.Count; c++)
+                            {
+                                text.text += dt.Columns[c].ColumnName + "=" +
+                                dr[c].ToString() + " ";
+                            }
+                            text.text += "\n";
+                            row++;
+                        }
+                    }*/
+
+        //print all from StarShip TABLE
+        // var results = dbManager.Query<SimpleSQL.Demos.>("SELECT * FROM StarShip");
+        /*
+                TextOutput.text = "";
+                foreach (var result in results)
+                {
+                    TextOutput.text += result.PlayerName + "\n";
+                    TextOutput.text += result.Range + "\n";
+
+                }
+                */
+        /*
+            sql = "CREATE INDEX \"StarShip_StarShipName\" on \"StarShip\"(\"StarShipName\")";
             dbManager.Execute(sql);
 
 
-        string sql1 = "INSERT INTO Players " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
-        dbManager.Execute(sql1, 1, "Nikolaos Sergis", 12f);
-        string sql2 = "INSERT INTO Players " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
-        dbManager.Execute(sql2, 2, "Lefteris Xenos", 22f);
-        string sql3 = "INSERT INTO Players " + "(PlayerID, PlayerName, Score) " + "VALUES (?, ?, ?)";
-        dbManager.Execute(sql3, 3, "Konstantinos Doldoukis", 2f);
+            string sql3 = "INSERT INTO StarShip " + "(StarShipID, StarShipName, HomePlanet, Range, Armor, Firepower) " + "VALUES (?, ?, ?, ?, ?, ?)";
+            dbManager.Execute(sql3, 40, "Enterprize", "Ermis", 12f, 50f, 60f);
 
-/*
-        int row = 0;
-        TextOutput.text = ""; 
-        Players dt = dbManager.Query("SELECT " +
-        "W.WeaponID, " +
-        "W.WeaponName, " +
-        "W.Damage, " +
-        "W.Cost, " +
-        "W.Weight, " +
-        "W.WeaponTypeID, " +
-        "T.Description AS
-        WeaponTypeDescription " +
-        "FROM " +
-        "Weapon W " +
-        "JOIN WeaponType T " +
-        "ON W.WeaponTypeID =
-        T.WeaponTypeID " +
-        "ORDER BY " +
-        "W.WeaponID ");
-                foreach (DataRow dr in dt.Rows)
-                {
-                    text.text += "Row: " + row.ToString() + " ";
-                    for (int c = 0; c < dt.Columns.Count; c++)
-                    {
-                        text.text += dt.Columns[c].ColumnName + "=" +
-                        dr[c].ToString() + " ";
-                    }
-                    text.text += "\n";
-                    row++;
-                }
-            }*/
+            string sql4 = "INSERT INTO StarShip " + "(StarShipID, StarShipName, HomePlanet, Range, Armor, Firepower) " + "VALUES (?, ?, ?, ?, ?, ?)";
+            dbManager.Execute(sql4, 50, "SersSpaceship", "Ermis", 17f, 50f, 60f);
 
-    //print all from StarShip TABLE
-    // var results = dbManager.Query<SimpleSQL.Demos.>("SELECT * FROM StarShip");
-    /*
+            //print all from StarShip TABLE
+            var results = dbManager.Query<SimpleSQL.Demos.StarShip>("SELECT * FROM StarShip");
+
             TextOutput.text = "";
             foreach (var result in results)
             {
-                TextOutput.text += result.PlayerName + "\n";
+                TextOutput.text += result.StarShipName + "\n";
                 TextOutput.text += result.Range + "\n";
 
             }
-            */
-    /*
-        sql = "CREATE INDEX \"StarShip_StarShipName\" on \"StarShip\"(\"StarShipName\")";
-        dbManager.Execute(sql);
+        */
 
 
-        string sql3 = "INSERT INTO StarShip " + "(StarShipID, StarShipName, HomePlanet, Range, Armor, Firepower) " + "VALUES (?, ?, ?, ?, ?, ?)";
-        dbManager.Execute(sql3, 40, "Enterprize", "Ermis", 12f, 50f, 60f);
+        // dbManager.CreateTable<Players>();
 
-        string sql4 = "INSERT INTO StarShip " + "(StarShipID, StarShipName, HomePlanet, Range, Armor, Firepower) " + "VALUES (?, ?, ?, ?, ?, ?)";
-        dbManager.Execute(sql4, 50, "SersSpaceship", "Ermis", 17f, 50f, 60f);
+        /* string sql = "INSERT INTO Player " +
+         "(nameid, playername, score) " +
+         "VALUES (?, ?, ?)";
+         dbManager.Execute(sql, 1, "nikos", 50);*/
 
-        //print all from StarShip TABLE
-        var results = dbManager.Query<SimpleSQL.Demos.StarShip>("SELECT * FROM StarShip");
-
-        TextOutput.text = "";
-        foreach (var result in results)
+        /*Players.Player playerstuff = new Players()
         {
-            TextOutput.text += result.StarShipName + "\n";
-            TextOutput.text += result.Range + "\n";
-
-        }
-    */
+            nameid = 1, playername = "nikos", score = 500};
+        dbManager.Insert(playerstuff);*/
 
 
-    // dbManager.CreateTable<Players>();
-
-    /* string sql = "INSERT INTO Player " +
-     "(nameid, playername, score) " +
-     "VALUES (?, ?, ?)";
-     dbManager.Execute(sql, 1, "nikos", 50);*/
-
-    /*Players.Player playerstuff = new Players()
-    {
-        nameid = 1, playername = "nikos", score = 500};
-    dbManager.Insert(playerstuff);*/
-
-
-}
+    }
 
 
 }
---
+++ Assets/Scripts/dotNet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class dotNet : MonoBehaviour
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("NIKOS");
+        string str = Console.ReadLine();
+        Console.WriteLine("Hello there " + str);
+
+    }
+}
